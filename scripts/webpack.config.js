0de3a3a1ed0b61eee2ec41b77703f33e655b383d
liaowei <l.w.kampfer@gmail.com>
2020-01-15 15:41:42 +0800

    支持react

--- scripts/webpack.config.js
+++ scripts/webpack.config.js
@@ -7,7 +7,10 @@ module.exports = {
             exclude: /(node_modules|bower_components)/,
             loader: 'babel-loader',
             options: {
-                presets: ['@babel/preset-env'],
+                presets: [
+                    '@babel/preset-env',
+                    '@babel/preset-react'
+                ],
                 plugins: ['@babel/plugin-transform-runtime'],
                 cacheDirectory: true
             }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './gltf';
 
-alert(1);
+ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('root')
+);
