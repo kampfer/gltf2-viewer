a25a51a231e1f2cb9fa24afbb1fd63946628d16e
liaowei <l.w.kampfer@gmail.com>
2020-02-04 14:55:14 +0800

    增加"npm run electron-dev"

---
+++ scripts/electronDev.js
@@ -0,0 +1,23 @@
+const { spawn } = require('child_process');
+const path = require('path');
+const webpack = require('webpack');
+const electron = require('electron');
+const devConfig = require('./webpack.dev.config');
+
+const compiler = webpack(devConfig);
+
+compiler.watch({
+    aggregateTimeout: 300
+}, function (err, stats) {
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+    console.log(stats.toString({
+        chunks: false,  // Makes the build much quieter
+        colors: true    // Shows colors in the console
+    }));
+});
+
+spawn(electron, [path.join(__dirname, '../src/electron.js')]);
