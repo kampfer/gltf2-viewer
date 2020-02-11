7f9597ba176ee516402f967fd4f18fe3e5548b73
liaowei <l.w.kampfer@gmail.com>
2020-02-11 15:34:13 +0800

    1、显示一些模型信息：object数量和顶点数量； 2、增加打开按钮

--- src/StatusBar/index.js
+++ src/StatusBar/index.js
@@ -4,14 +4,37 @@ import './index.less';
 
 export default class StatusBar extends React.Component {
 
+    makeInfo(gltf) {
+        let ret = {
+                vertices: 0,
+                faces: 0,
+                triangles: 0,
+                objects: 0
+            };
+
+        if (gltf) {
+            let scene = gltf.scenes[gltf.scene];
+            scene.traverse(function (child) {
+                ret.objects ++;
+                if (child.geometry) {
+                    ret.vertices += child.geometry._attributes.position.count;
+                }
+            });
+        }
+
+        return ret;
+    }
+
     render() {
+        let gltf = this.props.gltf,
+            info = this.makeInfo(gltf);
         return (
             <div className="status-bar bg-color-black-1">
                 <ul className="info">
-                    <li><span className="info-key">Vertices:</span><span className="info-value">0</span></li>
-                    <li><span className="info-key">Faces:</span><span className="info-value">0</span></li>
                     <li><span className="info-key">Triangles:</span><span className="info-value">0</span></li>
-                    <li><span className="info-key">Objects:</span><span className="info-value">0</span></li>
+                    <li><span className="info-key">Faces:</span><span className="info-value">0</span></li>
+                    <li><span className="info-key">Vertices:</span><span className="info-value">{info.vertices}</span></li>
+                    <li><span className="info-key">Objects:</span><span className="info-value">{info.objects}</span></li>
                 </ul>
             </div>
         );
