11d96739473ad2389d2b789b905891b0edb0e744
liaowei <l.w.kampfer@gmail.com>
2020-03-06 11:20:03 +0800

    尝试通过读取search中的字段gltf来加载远程gltf文件

--- src/GltfLoader/FileLoader.js
+++ src/GltfLoader/FileLoader.js
@@ -1,22 +1,31 @@
 import path from 'path';
 
+function isAbsolutePath(url) {
+    return /^https?:\/\//.test(url) || url.charAt(0) === '/';
+}
+
 export default class GLTFLoader {
 
-    constructor() {
+    constructor({
+        baseUrl
+    } = {}) {
+        this.baseUrl = baseUrl;
         this._requstingPool = {};
-    }
-
-    setBaseUrl(url) {
-        this._baseUrl = url;
     }
 
     setURLModifier(modifier) {
         this._urlModifier = modifier;
     }
 
+    // 本地文件的路径格式： filename.gltf
+    // 网络文件的路径格式： http(s)://path/to/gltf || path/to/gltf
     load(url) {
         if (url in this._requstingPool) {
             return this._requstingPool[url];
+        }
+
+        if (!isAbsolutePath(url) && this.baseUrl) {
+            url = `${this.baseUrl}/${url}`;
         }
 
         let modifiedUrl = url;
--- src/GltfLoader/index.js
+++ src/GltfLoader/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import FileLoader from './FileLoader';
 import GLTFParser from './GLTFParser';
+import path from 'path';
 
 import './index.less';
 
@@ -23,19 +24,6 @@ export default class FileReader extends React.Component {
         this.handleInputChnage = this.handleInputChnage.bind(this);
 
         this.fileInput = React.createRef();
-    }
-
-    render() {
-        let props = this.props;
-        return (
-            <div className={`file-uploader ${props.hide ? 'hide' : ''}`}>
-                <svg className="file-uploader__icon" xmlns="http://www.w3.org/2000/svg" width="100" height="86" viewBox="0 0 50 43">
-                    <path d="M48.4 26.5c-.9 0-1.7.7-1.7 1.7v11.6h-43.3v-11.6c0-.9-.7-1.7-1.7-1.7s-1.7.7-1.7 1.7v13.2c0 .9.7 1.7 1.7 1.7h46.7c.9 0 1.7-.7 1.7-1.7v-13.2c0-1-.7-1.7-1.7-1.7zm-24.5 6.1c.3.3.8.5 1.2.5.4 0 .9-.2 1.2-.5l10-11.6c.7-.7.7-1.7 0-2.4s-1.7-.7-2.4 0l-7.1 8.3v-25.3c0-.9-.7-1.7-1.7-1.7s-1.7.7-1.7 1.7v25.3l-7.1-8.3c-.7-.7-1.7-.7-2.4 0s-.7 1.7 0 2.4l10 11.6z" />
-                </svg>
-                <input className="file-uploader__file" type="file" id="file-uploader__file" ref={this.fileInput} multiple onChange={this.handleInputChnage} />
-                <label htmlFor="file-uploader__file"><strong>选择文件</strong><span className="box__dragndrop">或者将文件拖动到这里</span>.</label>
-            </div>
-        );
     }
 
     stopEvent(e) {
@@ -124,6 +112,38 @@ export default class FileReader extends React.Component {
 
     chooseGltf() {
         this.fileInput.current.click();
+    }
+
+    loadGltfFromUrl(url) {
+        let fileLoader = new FileLoader({baseUrl: path.dirname(url)}),
+            gltfParser = new GLTFParser({loader: fileLoader});
+
+        return fileLoader.load(url).then(function (json) {
+                return gltfParser.parse(json);
+            });
+    }
+
+    componentDidMount() {
+        let match = location.search.match(/gltf=([^&]*)/),
+            callback = this.props.onSuccess;
+
+        if (match && callback) {
+            this.loadGltfFromUrl(match[1]).then(callback);
+        }
+    }
+
+    render() {
+        let props = this.props;
+        return (
+            <div className={`file-uploader ${props.hide ? 'hide' : ''}`}>
+                <svg className="file-uploader__icon" xmlns="http://www.w3.org/2000/svg" width="100" height="86" viewBox="0 0 50 43">
+                    <path d="M48.4 26.5c-.9 0-1.7.7-1.7 1.7v11.6h-43.3v-11.6c0-.9-.7-1.7-1.7-1.7s-1.7.7-1.7 1.7v13.2c0 .9.7 1.7 1.7 1.7h46.7c.9 0 1.7-.7 1.7-1.7v-13.2c0-1-.7-1.7-1.7-1.7zm-24.5 6.1c.3.3.8.5 1.2.5.4 0 .9-.2 1.2-.5l10-11.6c.7-.7.7-1.7 0-2.4s-1.7-.7-2.4 0l-7.1 8.3v-25.3c0-.9-.7-1.7-1.7-1.7s-1.7.7-1.7 1.7v25.3l-7.1-8.3c-.7-.7-1.7-.7-2.4 0s-.7 1.7 0 2.4l10 11.6z" />
+                </svg>
+                <input className="file-uploader__file" type="file" id="file-uploader__file" ref={this.fileInput} multiple onChange={this.handleInputChnage} />
+                <label htmlFor="file-uploader__file"><strong>选择文件</strong><span className="box__dragndrop">或者将文件拖动到这里</span>.</label>
+                {props.children}
+            </div>
+        );
     }
 
 }
