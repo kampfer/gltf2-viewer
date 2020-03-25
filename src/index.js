8cb0a620196e1f33159cd5d2513d051824ba37b8
liaowei <l.w.kampfer@gmail.com>
2020-03-25 12:06:39 +0800

    使用微软配色方案

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@
 // https://github.com/KhronosGroup/glTF-Sample-Models/tree/master/2.0
 // https://github.com/KhronosGroup/glTF/blob/master/specification/2.0/README.md#properties-reference
 
-import './main.less';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
